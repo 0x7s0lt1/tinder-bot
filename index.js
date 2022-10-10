cfe7063eb2981f07b4beec1b087f293efa85e313
zsoltfehervari <zsoltfehervari@gmx.com>
2022-10-10 21:50:46 +0200

    try tf

--- index.js
+++ index.js
@@ -3,36 +3,80 @@
 require('dotenv').config();
 const cron = require("node-cron");
 const chalk =  require('chalk'); // chalk@4.1.2 
+const fs = require('fs');
+const tf = require('@tensorflow/tfjs-node');
+const use = require('@tensorflow-models/universal-sentence-encoder');
 
+// variables
 const UPDATE_INTERVAL = "*/5 * * * *";
 const LIKEING_INTERVAL = "* */4 * * *";
 let TINDER_API,MATCHES,U_MATCHES;
 
-//crons
+//cron modules
 const { swipeAllRight } = require('./cron/swipeAllRight');
 const { getMatches } = require('./cron/getMaches');
 const { processChanges } = require('./src/processChanges');
-
 
 //auth modules
 const { getFacebookToken } = require('./src/auth/getFacebookToken');
 const { getTinderToken } = require('./src/auth/getTinderToken');
 
 
+// tensoreflow object that can travel between the modules
+let TF = {
+    tf : tf,
+    encoder: null,
+    model : null,
+    data : {
+        lines : {},
+        questions : [],
+        responses : [],
+    }
+}
+  
+//cleaning data 
+const movie_lines = fs.readFileSync('./src/datasets/movie_lines.txt', 'utf8');
+movie_lines.split( "\n" ).forEach( l => {
+    let parts = l.split( " +++$+++ " );
+    TF.data.lines[ parts[ 0 ] ] = parts[ 4 ];
+});
+
+const movie_conversations = fs.readFileSync('./src/datasets/movie_conversations.txt', 'utf8');
+movie_conversations.split( "\n" ).forEach( c => {
+    let parts = c.split( " +++$+++ " );
+    if( parts[ 3 ] ) {
+        let phrases = parts[ 3 ].replace(/[^L0-9 ]/gi, "").split( " " ).filter( x => !!x );
+        for( let i = 0; i < phrases.length - 1; i++ ) {
+            TF.data.questions.push( TF.data.lines[ phrases[ i ] ] );
+            TF.data.responses.push( TF.data.lines[ phrases[ i + 1 ] ] );
+        }
+    }
+});
+
+// Initialize 
 async function init(){
 
-    console.log(chalk.bgGreen(new Date().toLocaleString(),'Tinder-Bot running on port: ' + PORT ));
+    console.log(chalk.bgGreen(new Date().toLocaleString(),'Tinder-Bot running!' ));
 
+    // load tf model and encoder
+    let TF = {
+        encoder: await use.load(),
+        model : await use.loadQnA()
+    };
+    
+
+    // getting authentications
     let fb_access_token = await getFacebookToken();
     TINDER_API = await getTinderToken(fb_access_token);
     let update  = await getMatches(TINDER_API);
     MATCHES = update.matches;
    
+    //init crons
     cron.schedule(LIKEING_INTERVAL,async ()=>{ await swipeAllRight(TINDER_API)});
     cron.schedule(UPDATE_INTERVAL,async ()=>{ 
         U_MATCHES = await getMatches(TINDER_API);
         if(MATCHES != U_MATCHES){
-            await processChanges(TINDER_API,U_MATCHES);
+            await processChanges(TINDER_API,U_MATCHES,TF);
             MATCHES = U_MATCHES;
         }else{
             console.log(chalk.bgGray( new Date().toLocaleString() + 'UPDATE-NO-CHANGES'));
--- src/processChanges.js
+++ src/processChanges.js
@@ -3,13 +3,17 @@
 require('dotenv').config();
 const chalk = require('chalk');
 
-//functions 
- const { sendMessage } = require('./functions/sendMessage');
 
- //data
+const NUM_SAMPLES = 200;
+
+//functions 
+const { sendMessage } = require('./functions/sendMessage');
+
+//data
 const  MSGSL  = require('./json/messages_list.json');
 
-async function processChanges(API_TOKEN,MATCHES){
+
+async function processChanges(API_TOKEN,MATCHES,TF){
  
 
     MATCHES.forEach( async ( match ) => {
@@ -18,31 +22,57 @@ async function processChanges(API_TOKEN,MATCHES){
           id : match._id,
           action : null,
           content : null
-      }
+      };
 
       // if new match
        if(match.messages.length === 0){
-          let first_question = MSGSL.first_questions[Math.floor(Math.random() * (MSGSL.first_questions.length - 1))];
+          let first_question = MSGSL.first_questions[Math.floor(Math.random() * MSGSL.first_questions.length )];
 
           MESSAGE.action = 'FIRST_MESSAGE';
           MESSAGE.content =`Hello ${match.person.name}!😊 ${first_question}`;
+
        };
 
        // if last message not from the Bot
        if(match.messages[match.messages.length - 1].from !== process.env.TINDER_UID){
 
+          //find last messages and concat them
           let msg_array = [];
+          // revers messages..so it starts wit the latest message
           match.messages = match.messages.reverse();
           for( let i = 0; i < match.messages.length; i++ ){
+            // if we find the first message from the bot break from loop
             if(match.messages[i].from === process.env.TINDER_UID){
               break;
             }
+            //esle collect message 
             msg_array.push(match.messages[i].message);
           }
 
-          let msg_string = msg_array.reverse().join(' ');
+          // reverse back and concat the messages into single string
+          let text = msg_array.reverse().join(' ');
 
-          //TODO: analyze messages and create reply 
+          let randomOffset = Math.floor( Math.random() * TF.data.questions.length );
+          const input = {
+            queries: [ text ],
+            responses: TF.data.questions.slice( randomOffset, NUM_SAMPLES )
+          };
+          let embeddings = await TF.model.embed( input );
+
+          TF.tf.tidy( () => {
+
+            const embed_query = embeddings[ "queryEmbedding" ].arraySync();
+            const embed_responses = embeddings[ "responseEmbedding" ].arraySync();
+            let scores = [];
+            embed_responses.forEach( response => {
+                scores.push( dotProduct( embed_query[ 0 ], response ) );
+            });
+            let id = scores.indexOf( Math.max( ...scores ) );
+            MESSAGE.content = responses[ randomOffset + id ];
+          });
+
+          embeddings.queryEmbedding.dispose();
+          embeddings.responseEmbedding.dispose();
 
        }
 
@@ -57,13 +87,6 @@ async function processChanges(API_TOKEN,MATCHES){
 
    
 }
-
-function normalize(value, min, max) {
-    if (min === undefined || max === undefined) {
-      return value;
-    }
-    return (value - min) / (max - min);
-  }
   
 
 module.exports = { processChanges }
